c40fbb8701be5a93c555ad8c0fc94855b7573741
Liu Xiaoyi <circuitcoder0@gmail.com>
2018-12-09 01:10:12 +0800

    Prev & Next

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 
-import { setRepeat } from './store/actions';
+import { setRepeat, playEntry } from './store/actions';
 
 import { artwork, music } from './util';
 
@@ -18,14 +18,29 @@ import {
 
 import './Root.scss';
 
+function findNext({ playing: { list: { entries }, index }, repeating }) {
+  if(!repeating && index === entries.length - 1) return null;
+  return (index + 1) % entries.length;
+}
+
+function findPrev({ playing: { list: { entries }, index }, repeating }) {
+  if(!repeating && index === 0) return null;
+  if(index === 0) return entries.length - 1;
+  else return index - 1;
+}
+
 const mapS2P = state => ({
   playing: state.playing,
   playingEntry: state.playing ? state.playing.list.entries[state.playing.index] : null,
   repeating: state.repeating,
+
+  next: state.playing ? findNext(state) : null,
+  prev: state.playing ? findPrev(state) : null,
 });
 
 const mapD2P = dispatch => ({
   setRepeat: repeat => dispatch(setRepeat(repeat)),
+  playEntry: (list, index) => dispatch(playEntry(list, index)),
 });
 
 class Root extends React.PureComponent {
@@ -78,6 +93,16 @@ class Root extends React.PureComponent {
     audio.play();
   }
 
+  prev() {
+    if(this.props.prev !== null)
+      this.props.playEntry(this.props.playing.list, this.props.prev);
+  }
+
+  next() {
+    if(this.props.next !== null)
+      this.props.playEntry(this.props.playing.list, this.props.next);
+  }
+
   updateProgress() {
     const audio = this.audio.current;
     const progress = audio.currentTime / audio.duration;
@@ -86,7 +111,7 @@ class Root extends React.PureComponent {
   }
 
   render() {
-    const { playing, repeating, setRepeat, playingEntry } = this.props;
+    const { playing, repeating, setRepeat, playingEntry, prev, next } = this.props;
     const { progress, paused } = this.state;
 
     return (
@@ -104,13 +129,13 @@ class Root extends React.PureComponent {
                 <div className="playing">
                   <div className="bottom-artwork" style={{backgroundImage: `url(${artwork(playingEntry)})`}}></div>
                   <div className="control">
-                    <Icon>skip_previous</Icon>
+                    <Icon onClick={() => this.prev()} className={prev === null ? 'disabled' : ''}>skip_previous</Icon>
                     { paused ? 
                         <Icon onClick={() => this.play()}>play_arrow</Icon>
                         :
                         <Icon onClick={() => this.pause()}>pause</Icon>
                     }
-                    <Icon>skip_next</Icon>
+                    <Icon onClick={() => this.next()} className={next === null ? 'disabled' : ''}>skip_next</Icon>
                     <div className="spacer"></div>
                     <Icon onClick={() => setRepeat(!repeating)}>{ repeating ? 'repeat' : 'trending_flat' }</Icon>
                     <NavLink to={`/${playing.list._id}`}><Icon>queue_music</Icon></NavLink>
